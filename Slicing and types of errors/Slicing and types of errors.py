8f93f7ba48a5a95e7b6f2257f0823d1583814d0c
sonicgauchan10 <126335102+sonicgauchan10@users.noreply.github.com>
2024-12-21 18:11:01 +0545

    Add files via upload

---
+++ Slicing and types of errors/Slicing and types of errors.py
@@ -0,0 +1,117 @@
+#1
+a= int(input("Enter a number:"))
+b= int(input("Enter another number:"))
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")   
+    
+#2
+a= int(input("Enter a number:"))
+if a % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd:")    
+
+#3
+a= int(input("Enter a number(1-12):"))
+if a == 1:
+    print("Janauary")
+elif a == 2:
+    print("February")  
+elif a == 3:
+    print("March") 
+elif a == 4:
+    print("April")
+elif a == 5:
+   print("May") 
+elif a == 6:
+    print("June") 
+elif a == 7:
+    print("July") 
+elif a == 8:
+    print("August") 
+elif a == 9:
+    print("September") 
+elif a == 10:
+    print("October") 
+elif a == 11:
+    print("November") 
+elif a == 12:
+    print("December") 
+else:
+    print("Error")    
+
+#4 
+a= int(input("Enter your marks:"))
+if a > 80:
+    print("Grade A")
+elif a > 60 and a <= 80:
+    print("Grade B")
+elif a > 50 and a <= 60:
+    print("Grade C")
+elif a > 45 and a <= 50:
+    print("Grade D")
+elif a > 25 and a <= 45:
+    print("Grade E")
+else:
+    print("Grade F")                        
+ 
+#5
+a= int(input('Enter a number:'))
+if a % 7 == 0:
+    print(f"{a} is divisible by 7.")
+else:
+    print(f"{a} is not divisible by 7.")    
+
+#6
+a= int(input('Enter one number:'))
+b= int(input('Enter another number:'))
+x= input('Enter a operator:')
+if x == '+':
+    print(a+b)
+elif x == '-':
+    print(a-b)
+else:
+    print("Error")        
+
+#7
+a= int(input("Enter a number:"))
+if a % 5 == 0:
+    print("Hello")
+else:
+    print("Bye") 
+
+#8
+a= int(input('Enter a number: '))
+if a % 3 == 0 and a % 5 == 0:
+    print("FizzBuzz")
+elif a % 5 == 0:
+    print("Buzz")
+elif a % 3 == 0:
+    print("Fizz")
+else:    
+    print(a)                
+     
+#9
+char= input("Enter a charater: ")
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    print("Vowel")
+else:
+    print("Consonant")    
+
+#10
+#11
+#12
+#13
+#14
+#15
+#16
+#17
+#18
+#19
+#20
+#21
+#22         
